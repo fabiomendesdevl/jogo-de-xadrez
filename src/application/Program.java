aac2d445ab9fe664aad05e9494368dd045bde527
Fabio Mendes <fabiomendesdev@outlook.com>
2024-03-16 10:56:00 -0300

    Classe: Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Posicao pos = new Posicao(1, 3);
+		
+		System.out.println(pos);
 	}
 
 }
